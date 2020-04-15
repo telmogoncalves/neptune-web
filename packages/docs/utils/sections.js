8287666cb069a081b605b500b3051aeafdf469bf
Naiara Abaroa <naiara.abaroa@transferwise.com>
2020-04-15 07:47:05 +0100

    feat: add custom media tokens documentation

--- packages/docs/utils/sections.js
+++ packages/docs/utils/sections.js
@@ -1,5 +1,6 @@
 export default [
   { title: 'Getting started', dir: '' },
+  { title: 'Tokens', dir: 'tokens' },
   { title: 'React Components', dir: 'components' },
   { title: 'CSS Components', dir: 'css' },
 ];
