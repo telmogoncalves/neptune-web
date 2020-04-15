637ea02a5708e39931fc8d1b5570907c2afec66d
Naiara Abaroa <naiara.abaroa@transferwise.com>
2020-04-15 07:27:56 +0100

    feat: add spacing documentation

--- packages/docs/utils/sections.js
+++ packages/docs/utils/sections.js
@@ -1,6 +1,7 @@
 export default [
   { title: 'Getting started', dir: '' },
   { title: 'Foundation', dir: 'foundation' },
+  { title: 'Addons', dir: 'addons' },
   { title: 'React Components', dir: 'components' },
   { title: 'CSS Components', dir: 'css' },
 ];
