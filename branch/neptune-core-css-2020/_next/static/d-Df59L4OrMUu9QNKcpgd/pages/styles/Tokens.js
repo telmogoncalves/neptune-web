7266a0ad277c8d77c211c9d76e1a740b2899bc72
CircleCI <circleci@transferwise.com>
2020-04-21 09:31:34 +0000

    Updates

---
+++ branch/neptune-core-css-2020/_next/static/d-Df59L4OrMUu9QNKcpgd/pages/styles/Tokens.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[50],{eO33:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/styles/Tokens",function(){return o("PZS0")}])}},[["eO33",0,1]]]);
