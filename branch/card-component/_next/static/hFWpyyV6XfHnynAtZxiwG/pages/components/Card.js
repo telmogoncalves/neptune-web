369d01272c19775fae1235c077064d16d258c4bc
CircleCI <circleci@transferwise.com>
2020-04-23 17:02:51 +0000

    Updates

---
+++ branch/card-component/_next/static/hFWpyyV6XfHnynAtZxiwG/pages/components/Card.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"XXU+":function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Card",function(){return w("8t5b")}])}},[["XXU+",0,1]]]);
