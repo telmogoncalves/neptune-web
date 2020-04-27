dfd6f642dee07718d4ceb56ed1b7cbf5f4b749fe
CircleCI <circleci@transferwise.com>
2020-04-27 15:12:28 +0000

    Updates

---
+++ branch/card-component/_next/static/uwCof3_ZLIdLyLftFGyJq/pages/components/Card.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{"XXU+":function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Card",function(){return w("8t5b")}])}},[["XXU+",0,1,4,7,2,3,5,6]]]);
---
+++ branch/card-component/_next/static/uwCof3_ZLIdLyLftFGyJq/pages/components/Field.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{FWqW:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Field",function(){return w("lQrX")}])}},[["FWqW",0,1,4,7,2,3,5,6]]]);
