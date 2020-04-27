5eed1327666af4f95b4176dc051b979a9056bbdc
CircleCI <circleci@transferwise.com>
2020-04-27 16:32:53 +0000

    Updates

---
+++ branch/neptune-core-css-docs-2020/_next/static/QftH8iKagJF-XM5o9B2xM/pages/index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[58],{Sjde:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return o("xEgi")}])}},[["Sjde",0,1,6,7,3,2,4,5]]]);
