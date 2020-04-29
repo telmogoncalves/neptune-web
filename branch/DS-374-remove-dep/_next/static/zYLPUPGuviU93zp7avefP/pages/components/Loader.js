54a1587dc924ea78d46daaf3d10e233979010a79
CircleCI <circleci@transferwise.com>
2020-04-29 13:49:17 +0000

    Updates

---
+++ branch/DS-374-remove-dep/_next/static/zYLPUPGuviU93zp7avefP/pages/components/Loader.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[37],{SzNQ:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Loader",function(){return w("mfvS")}])}},[["SzNQ",0,1,4,5,2,3,6,7]]]);
