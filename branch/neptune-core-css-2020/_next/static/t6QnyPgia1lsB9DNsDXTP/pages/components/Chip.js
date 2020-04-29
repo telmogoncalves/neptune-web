818b276495e006202eeb5bd84fd25d6ca07b8af1
CircleCI <circleci@transferwise.com>
2020-04-29 08:17:01 +0000

    Updates

---
+++ branch/neptune-core-css-2020/_next/static/t6QnyPgia1lsB9DNsDXTP/pages/components/Chip.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{"LrS+":function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Chip",function(){return w("L9Bz")}])}},[["LrS+",0,1,4,5,2,3,6,7]]]);
