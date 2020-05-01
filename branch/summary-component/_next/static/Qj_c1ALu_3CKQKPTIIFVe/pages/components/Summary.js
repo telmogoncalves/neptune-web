bcd6f096753faa222d96dca5269d9b4fa9991933
CircleCI <circleci@transferwise.com>
2020-05-01 14:27:36 +0000

    Updates

---
+++ branch/summary-component/_next/static/Qj_c1ALu_3CKQKPTIIFVe/pages/components/Summary.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{XHeJ:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Summary",function(){return w("mcgz")}])}},[["XHeJ",0,1,4,5,2,3,6,7]]]);
