a0db2c1cad968d6ecee8da6b438c6d3d5e8ad6a8
CircleCI <circleci@transferwise.com>
2020-05-01 11:40:43 +0000

    Updates

---
+++ branch/improve-PR-template/_next/static/hRs4ZGM__BfrBZM1GeFPP/pages/components/Loader.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{SzNQ:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Loader",function(){return w("mfvS")}])}},[["SzNQ",0,1,4,7,2,3,5,6]]]);
